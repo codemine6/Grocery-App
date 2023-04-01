295a6727bf49d3c8e1bc9e35f90191b67a5a6888
codemine6 <codemine6@gmail.com>
2023-04-01 11:40:07 +0700

    Initial commit

---
+++ lib/views/home/bundle_create_page.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+import '../../core/components/app_back_button.dart';
+import '../../core/constants/constants.dart';
+import 'components/bundle_create_bottom_action_bar.dart';
+import 'components/bundle_create_food_categories.dart';
+import 'components/bundle_create_search_bar.dart';
+import 'components/product_grid_view.dart';
+
+class BundleCreatePage extends StatelessWidget {
+  const BundleCreatePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: const AppBackButton(),
+        title: const Text('Create My Pack'),
+      ),
+      body: Column(
+        children: const [
+          SearchBar(),
+          FoodCategories(),
+          SizedBox(height: AppDefaults.padding / 2),
+          ProductGridView(),
+          BottomActionBar(),
+        ],
+      ),
+    );
+  }
+}
